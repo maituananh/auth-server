fad715efbc400b79dd763bc8e00ff648b6a27309
anhmt <maituananh12061998@gmail.com>
2025-11-02 16:49:53 +0700

    Initial commit

---
+++ src/test/java/com/vn/anhmt/authentication/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.anhmt.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
